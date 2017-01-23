684f77c685a504087cb0e83b20a7048126774ae0
qpham01 <quoc_pham@msn.com>
2017-01-22 23:53:52 -0800

    snippets added.

--- sdcnd/CarND-Advanced-Lane-Lines/snippets.py
+++ sdcnd/CarND-Advanced-Lane-Lines/snippets.py
@@ -2,3 +2,6 @@
 forward_coords = np.float32([[610, 440], [678, 440], [1206, 670], [278, 670]])
 topdown_coords = np.float32([[320, 0], [960, 0], [960, 670], [320, 670]])
 
+# Coordinates from forward view and topdown view for making perspective transform
+forward_coords = np.float32([[610, 440], [678, 440], [1206, 670], [278, 670]])
+topdown_coords = np.float32([[200, 0], [1080, 0], [1080, 670], [200, 670]])
