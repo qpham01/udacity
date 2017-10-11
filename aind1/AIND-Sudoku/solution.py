5eff0f2a36ad636cd211a9794fa5ab19698e0183
Quoc Pham <quoc@simplify.ai>
2017-10-11 11:30:48 -0700

    Naked twins work

--- aind1/AIND-Sudoku/solution.py
+++ aind1/AIND-Sudoku/solution.py
@@ -1,15 +1,24 @@
 assignments = []
 rows = 'ABCDEFGHI'
 cols = '123456789'
+
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
 boxes = cross(rows, cols)
 
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-unitlist = row_units + column_units + square_units
+diag_units1 = [rows[i] + cols[i] for i, _ in enumerate(rows)]
+diag_units2 = [rows[i] + cols[8 - i] for i, _ in enumerate(rows)]
+print(diag_units1)
+print(diag_units2)
+unitlist = [diag_units1, diag_units2] + row_units + column_units + square_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
-
 
 def assign_value(values, box, value):
     """
@@ -34,13 +43,17 @@ def naked_twins(values):
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
-
+    for unit in row_units:
+        values = eliminate_naked_twins_numbers(unit, values)
+    for unit in column_units:
+        values = eliminate_naked_twins_numbers(unit, values)
+    for unit in square_units:
+        values = eliminate_naked_twins_numbers(unit, values)
+    values = eliminate_naked_twins_numbers(diag_units1, values)
+    values = eliminate_naked_twins_numbers(diag_units2, values)
+    return values
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
-
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    return [s+t for s in A for t in B]
 
 def grid_values(grid):
     """
@@ -53,8 +66,8 @@ def grid_values(grid):
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
     values = dict()
-    for i, label in enumerate(boxes):
-        values[label] = grid[i] if not grid[i] == '.' else '123456789'
+    for label in boxes:
+        values[label] = grid[label] if label in grid else '123456789'
 
     return values
 
@@ -83,17 +96,42 @@ def eliminate(values):
 
 def only_choice(values):
     for unit in unitlist:
-        for digit in '123456789':
+        for digit in '123456789':            
             dplaces = [box for box in unit if digit in values[box]]
-            if len(dplaces) == 1:
-                values[dplaces[0]] = digit
+            if len(dplaces) == 1 and len(values[dplaces[0]]) > 1:
+                #print("dplaces for {}: {}, {}".format(digit, dplaces, [values[box] for box in dplaces]))            
+                assign_value(values, dplaces[0], digit)
     return values
+
+def eliminate_naked_twins_numbers(unit, values):
+    """ Find a list of numbers in naked twins in a unit, if any """
+    twin_numbers = dict()
+    for box1 in unit:
+        box1_len = len(values[box1])
+        if box1_len == 2:
+            for box2 in unit:
+                if box1 == box2:
+                    continue
+                if values[box1] == values[box2]:
+                    for number in values[box1]:
+                        twin_numbers[number] = [box1, box2]
+    #new_values = values.copy()
+    for number in twin_numbers:
+        twin_boxes = twin_numbers[number]
+        non_twin_boxes = [box for box in unit if box not in twin_boxes]
+        for box in non_twin_boxes:
+            current_value = values[box]
+            test = current_value.replace(number, '')
+            assign_value(values, box, test)            
+    return values
+
 
 def reduce_puzzle(values):
     solved_values = [box for box in values.keys() if len(values[box]) == 1]
     stalled = False
     while not stalled:
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        #values = naked_twins(values)
         values = eliminate(values)
         values = only_choice(values)
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
@@ -111,7 +149,6 @@ def search(values):
         return values ## Solved!
     # Choose one of the unfilled squares with the fewest possibilities
     _, s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
-    print("s", s)
     # Now use recurrence to solve each one of the resulting sudokus, and
     for value in values[s]:
         new_sudoku = values.copy()
@@ -129,6 +166,27 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = grid_values(grid)
+    values = search(values)
+    if values:
+        solved_values = [box for box in values.keys() if len(values[box]) == 1]   
+        assert len(solved_values) == len(values)
+        # display(values)
+    else:
+        print("No solution found!")
+    return values
+
+def test_valid_solution(values):
+    for box in values:
+        assert len(values[box]) == 1
+        assert values[box] in '123456789'
+
+    for unit in unitlist:
+        for i, box1 in enumerate(unit):
+            for j, box2 in enumerate(unit):
+                if i != j:
+                    assert values[box1] != values[box2] 
+    return True
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
---
+++ aind1/AIND-Sudoku/test_sudoku.py
@@ -0,0 +1,64 @@
+""" Tests for Sudoku """
+
+import unittest
+from solution import display, grid_values, eliminate, only_choice, reduce_puzzle, search
+
+EASY_GRID = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+HARD_GRID = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+ALL_VALUES = '123456789'
+
+class SudokuTests(unittest.TestCase):
+    """ Tests for Sudoku """
+    def test_01_grid_values(self):
+        """ Test for grid_values method """
+        values = grid_values(EASY_GRID)
+        assert len(values) == 81
+        assert values['A1'] == ALL_VALUES
+        assert values['A2'] == ALL_VALUES
+        assert values['A3'] == '3'
+        assert values['I7'] == '3'
+        assert values['I8'] == ALL_VALUES
+        assert values['I9'] == ALL_VALUES
+
+    def test_02_eliminate(self):
+        """ Test eliminate method """
+        values = grid_values(EASY_GRID)
+        values = eliminate(values)
+        assert values['A1'] == '45'
+        assert values['A2'] == '4578'
+        assert values['A3'] == '3'
+        assert values['I7'] == '3'
+        assert values['I8'] == '24678'
+        assert values['I9'] == '2467'
+
+    def test_03_only_choice(self):
+        """ Tests only choice method """
+        values = grid_values(EASY_GRID)
+        values = eliminate(values)
+        for _ in range(0, 4):
+            values = only_choice(values)
+
+        assert values['A6'] == '1', "{}: {}".format('A6', values['A6'])
+        assert values['E2'] == '2', "{}: {}".format('C9', values['C9'])
+
+    def test_04_reduce_puzzle(self):
+        """ Tests reduce_puzzle """
+        values = grid_values(EASY_GRID)
+        values = reduce_puzzle(values)
+        solved_values = [box for box in values.keys() if len(values[box]) == 1]    
+        assert len(solved_values) == len(values)
+
+    def test_05_reduce_hard_puzzle(self):
+        """ Tests reduce_puzzle """
+        values = grid_values(HARD_GRID)
+        values = search(values)
+        if values:
+            solved_values = [box for box in values.keys() if len(values[box]) == 1]   
+            assert len(solved_values) == len(values)
+            display(values)
+        else:
+            print("No solution found!")
+
+
+if __name__ == '__main__':
+    unittest.main()
