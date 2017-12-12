aabcc6f2d339cd27c5d46ea22bc5c7594c9a16b0
Quoc Pham <quoc@simplify.ai>
2017-12-11 21:46:15 -0600

    Completed RNN project

---
+++ aind2/aind2-rnn/my_answers.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+import re
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Activation
+import keras
+
+# TODO: fill out the function below that transforms the input series 
+# and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_series(series, window_size):
+    # containers for input/output pairs
+    X = []
+    l = len(series)
+    for i in range(l - window_size):
+        X.append(series[i : i + window_size])
+
+    y = series[window_size:]
+
+    # reshape each
+    X = np.asarray(X)
+    X.shape = (np.shape(X)[0:2])
+    y = np.asarray(y)
+    y.shape = (len(y),1)
+
+    return X,y
+
+# TODO: build an RNN to perform regression on our time series input/output data
+def build_part1_RNN(window_size):
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    return model
+
+### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+def cleaned_text(text):
+    punctuation = ['!', ',', '.', ':', ';', '?']
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', \
+        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    l = len(text)
+    for i in range(l):
+        c = text[i]
+        if c not in alpha and c not in punctuation:
+           text = text.replace(c, ' ')
+           print("replaced {}".format(c))
+    return text
+
+### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_text(text, window_size, step_size):
+    # containers for input/output pairs
+    inputs = []
+    outputs = []
+
+    l = len(text)
+    for i in range(0, l - window_size, step_size):
+        if l - i < window_size:
+            break
+        inputs.append(text[i:i + window_size])
+        outputs.append(text[i + window_size])
+
+    return inputs,outputs
+
+# TODO build the required RNN model: 
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+def build_part2_RNN(window_size, num_chars):
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+    return model
